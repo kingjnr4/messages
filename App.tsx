82e2195c1e22a127e5ebe2ef1224df5c74e9ae1f
kingjnr4 <onyiboixy@gmail.com>
2023-07-26 14:49:38 +0100

    init

---
+++ App.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable react/style-prop-object */
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View className="fff">
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
